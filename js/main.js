08ce7645cca91a1ef702b009b52c63c8d26b7cf3
unknown <gabrieltorresmora2@gmail.com>
2022-10-08 19:09:25 -0300

    haciendo funcionar el input con la base de datos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,53 @@
-async function getData(){
-    let llamadaApi = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Cordoba&appid=fffc25dd0555327e5a0644d3cbc1749c'),
-       json = await llamadaApi.json();
-       
-       console.log(json)
+let contenedorClima = document.querySelector('.contenedor-clima'),
+    contenedorImagen = document.querySelector('.contenedor-imagen')
+
+const searchClima = event => {
+    event.preventDefault();
+    const { value } = event.target.weather;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value.toLowerCase()}&appid=fffc25dd0555327e5a0644d3cbc1749c`)
+    .then(data => data.json())
+    .then(response => renderClimaData(response))
 }
-getData();
+
+const renderClimaData = data => {
+    console.log(data)
+
+    contenedorClima.style.display = "flex";
+
+    /* Creando los elementos del Dom */
+    const card = document.createElement('div'),
+          name = document.createElement('h2'),
+          sensacionTermica = document.createElement('p'),
+          humedad = document.createElement('p'),
+          presion = document.createElement('p'),
+          clima = document.createElement('p'),
+          climaMax = document.createElement('p'),
+          climaMin = document.createElement('p');
+
+
+    /*Creando las clases de los elementos del DOM  */
+    card.classList.add('card')
+    name.classList.add('titulo-pais')
+    clima.classList.add('clima')
+    climaMax.classList.add('climaMax')
+    climaMin.classList.add('climaMin')
+    humedad.classList.add('humedad')
+    presion.classList.add('presion')
+    sensacionTermica.classList.add('sensacionTermica')
+
+    /* haciendo un appenchil para mostrar los elementos en la pagina */
+    name.textContent = data.name
+    clima.textContent = `Temperatura: ${(data.main.temp - 273.15).toFixed(1)} °C`
+    sensacionTermica.textContent = `Sensacion Termica: ${(data.main.feels_like -273.15).toFixed(1)} °C`
+
+
+    card.appendChild(sensacionTermica)
+    
+    contenedorImagen.appendChild(name)
+    contenedorImagen.appendChild(clima)
+
+    contenedorClima.appendChild(card)
+
+    
+}
+
